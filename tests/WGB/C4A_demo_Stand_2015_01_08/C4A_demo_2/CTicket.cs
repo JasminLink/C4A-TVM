5140c0ed435ada566f61dec371ece5443e711528
JasminLink <jasmin.link@iao.fraunhofer.de>
2015-01-12 17:48:10 +0100

    WebApps.txt + TicketUpdate

--- tests/WGB/C4A_demo_Stand_2015_01_08/C4A_demo_2/CTicket.cs
+++ tests/WGB/C4A_demo_Stand_2015_01_08/C4A_demo_2/CTicket.cs
@@ -15,11 +15,11 @@ namespace C4A_demo_2
     public class CTicket
     {
         public String destination = "";     // where to
-        public String origin = "";          // from
-        public String tarif = "";          // adult or child
+        public String origin = "";          // from - deprecated
+        public String tarif = "";           // adult or child
         public String date = "";            // date of validity
         public double price = 0.0;          // price
-        public String type = "";            // single or group ticket
+        public String type = "";            // single, multi journey, day
         public String special = "";         // special conditions such as tourist offers, week-end special
 
         public double basefare = 2.40;
