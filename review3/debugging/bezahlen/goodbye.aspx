2b5b23636a21f12ed1b2c3fb36e7b26bcd4f776d
Jasmin TVM <jasmin.link@iao.fraunhofer.de>
2015-01-17 17:25:39 +0100

    WGB bezahlen seiten

---
+++ review3/debugging/bezahlen/goodbye.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="goodbye.aspx.cs" Inherits="C4A_demo_2.goodbye" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head id="Head1" runat="server">
+    <title>Good Bye</title>
+    
+    
+<link rel="stylesheet" type="text/css" media="all" href="style.css" />
+<link rel="stylesheet" type="text/css" media="all" href="C4A_styles.css"/>
+
+<script type="text/javascript" src="helper.js"></script>
+<script type="text/javascript" src="../js/jquery-1.10.2.js"></script>
+<script type="text/javascript" src="../js/jquery-ui.js"></script>
+
+</head>
+
+<body onload="window.setTimeout('ZeitAnzeigen(\'fr\')', 1000)">
+<form id="form2" runat="server">
+<asp:Panel id="display" CssClass="startseite_C4A" style="width: 1024px; height:768px;" runat="server">
+
+    <div id="headline">
+    	<div id="headcontainer">
+		    <div id="zeiticon" runat="server" class="zeiticon"></div> 
+			<div id="Uhr" class="time" runat="server">&nbsp;</div>
+
+            <div id="statuszeile" runat="server" style="font-size: 9px" visible="false">&nbsp;</div>
+    	</div>
+    </div>
+
+    <div id="container">
+
+   
+      	<asp:ScriptManager ID="sm" runat="server" EnablePartialRendering="true"></asp:ScriptManager>
+       <asp:Timer runat="server" ID="timeout_ticker" Interval="5000" 
+    ontick="timeout_ticker_Tick"></asp:Timer>
+        
+    
+    <asp:Label id="goodbyemessage" CssClass="area_header" Text="Palim Palim" runat="server"></asp:Label>
+    <br />
+    <br />
+    <br />
+    <asp:Label id="goodbyemessage2" CssClass="area_header" Text="Palim Palim" runat="server"></asp:Label>
+    
+</div>
+    
+   
+</asp:Panel>
+</form> 
+</body>
+</html>
+
---
+++ review3/debugging/bezahlen/goodbye.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace C4A_demo_2
+{
+    public partial class goodbye : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            applyLanguage(Global.current_language);
+            serverside_apply_style(Global.current_theme);
+            // applyFontFace_clientside(Global.current_fontface);
+            applyFontFace(Global.current_fontface);
+            applyFontFace_clientside(Global.current_fontface);
+            
+            
+
+        }
+
+
+
+        public void applyFontFace(String family)
+        {
+
+
+            goodbyemessage.Attributes["style"] = "font-family: " + family;
+            goodbyemessage2.Attributes["style"] = "font-family: " + family;
+
+        }
+
+        public void applyFontFace_clientside(String family)
+        {
+            String arg = "myFunction('" + family + "');";
+            ScriptManager.RegisterStartupScript(this, GetType(), "myFunction", arg, true);
+        }
+
+        public void serverside_apply_style(String theme)
+        {
+
+            if (theme.Contains("normal"))
+            {
+                display.CssClass = "startseite_C4A";
+                goodbyemessage.CssClass = "area_header";
+                goodbyemessage2.CssClass = "area_header";
+            }
+
+
+
+            else if (theme.Contains("contrast"))
+            {
+                display.CssClass = "startseite_C4A_contrast";
+                goodbyemessage.CssClass = "area_header_contrast";
+                goodbyemessage2.CssClass = "area_header_contrast";
+            }
+
+
+
+        }
+
+        public void applyLanguage(String lang)
+        {
+            Global.fillWordingTable(lang);
+
+            goodbyemessage.Text = Global.wordingtable[41];
+            goodbyemessage2.Text = Global.wordingtable[42];
+      
+        }
+
+
+
+        protected void timeout_ticker_Tick(object sender, EventArgs e)
+        {
+            Global.current_language = "de";
+            Global.current_fontface = "Calibri";
+            Global.current_theme = "normal";
+            Global.set_timeout("normal");
+
+            Server.Transfer("Default.aspx");
+        }
+    }
+}
